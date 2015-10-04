034ecd9a704efc94d2b38a55f7fc1b8e2120cf4a
BMaxV <maxivoss@hotmail.de>
2015-10-04 16:44:46 +0200

    first commit

---
+++ pointmath.py
@@ -0,0 +1,29 @@
+#http://mathproofs.blogspot.de/2005/04/uniform-random-distribution-on-sphere.html
+
+import random
+import math
+
+def rand_surface_point():
+    
+    r1=random.random()
+    r2=random.random()
+    
+    angle1= r1 * 2 * math.pi
+    angle2= math.acos( 1 - 2* r2)
+    
+    #angle one is sideways, our x
+    #angle two is updown, our y
+    
+    return angle1,angle2
+    
+def to_xyz(angle1,angle2):
+    
+    a1,a2=rand_surface_point()
+    
+    #for a different radius just multiply all of these by r
+    x=math.sin(a1)*math.sin(a2)
+    y=math.cos(a1)*math.sin(a2)
+    z=math.cos(a2)
+    
+    return (x,y,z)
+    
